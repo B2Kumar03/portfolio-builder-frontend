fd98b45a4dfe7e56edb9e81dd3fb2e0286139f70
Bittu Kumar <bk7355583@gmail.com>
2024-06-17 13:54:17 +0530

    login done

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);//ad
 app.use("/api/v1",emailCheckRouter)
 app.use("/api/v1/users/",projectRouter)
 app.use("/api/v1/users",personalDatilsRoutes)
